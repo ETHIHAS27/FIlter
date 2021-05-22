518124334e5adb2a994f649276b2762c10c5017c
ETHIHAS27 <71704339+ETHIHAS27@users.noreply.github.com>
2021-05-22 19:05:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(500,400)
+
+video = createCapture(VIDEO)
+    video.hide();
+
+}
+
+function draw(){
+    image(video,0,0,500,400);
+}
+
+function take_snapshot(){
+    save('image.jpg')
+}
+
+function boy(){
+    document.getElementById('boy').style = 'background-color:red;'
+}
+
+function girl(){
+    document.getElementById('girl').style = 'background-color:red;'
+}
